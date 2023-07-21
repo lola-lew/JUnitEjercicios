1b1a2c18b7fbb62844c519b2062bf7fe6f1a1ebb
Loli <lolarlew@gmail.com>
2023-07-21 16:17:33 -0300

    pass validator incializado

---
+++ src/test/java/calcu/ej/PasswordValidatorTest.java
@@ -0,0 +1,12 @@
+package calcu.ej;
+
+import org.junit.Before;
+
+public class PasswordValidatorTest {
+  private PasswordValidator passValidator;
+
+  @Before
+  public void iniciar() {
+    passValidator = new PasswordValidator();
+  }
+}
